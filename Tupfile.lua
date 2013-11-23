2ec81c8593e12479c75b3d7d1578b9b2dab262c6
Jack Moffitt <jack@metajack.im>
2013-11-23 16:53:47 -0700

    wip3

--- Tupfile.lua
+++ Tupfile.lua
@@ -3,9 +3,9 @@ tup.include("rust.lua")
 STATIC_LIB_EXT = ".a"
 
 RUSTC = "rustc"
-RUSTFLAGS = ""
+RUSTFLAGS = "-Llibpng -Llibpng/zlib"
 
 tup.rule("shim.c", "gcc -o %o -c %f", "%B.o")
 tup.rule("shim.o", "ar crs %o %f", "libshim" .. STATIC_LIB_EXT)
 
-rust.library("lib.rs", {"libshim.a"})
+rust.library("lib.rs", {"libshim.a", "libpng/libpng.a", "libpng/zlib/libz.a"})
